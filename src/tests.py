f6e0edf5333fd99135ce0d6809ec69c5417887c2
Tomasz Janisiewicz <tomekjanisiewi@gmail.com>
2026-07-16 15:07:38 +0200

    added getting_members tests

--- src/tests.py
+++ src/tests.py
@@ -1,4 +1,5 @@
 from main import get_data
+from api import get_members
 
 
 """
@@ -6,4 +7,10 @@ GETTING AND SAVING DATA FROM SEJM API TESTS"
 """
 get_data(2026, 5)
 get_data(2026, 4)
-get_data(2026, 50)
+# get_data(2026, 50)
+
+
+"""
+GETTING MEMBERS
+"""
+get_members(10)
