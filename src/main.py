5c48317faeb80f0252211eb1fb1a56523f85e268
Tomasz Janisiewicz <tomekjanisiewi@gmail.com>
2026-07-16 18:28:25 +0200

    added get_members function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from api import get_data_format, get_voting
-from save_data import get_file_name, save
+from save_data import get_file_name, save, get_members_file_name
+from src.save_data import DATA_DIR
+import json
 
 TERM = 10
 
@@ -7,4 +9,9 @@ def get_data(year: int, month: int) -> None:
     date_from, date_to = get_data_format(year, month)
     data = get_voting(TERM, date_from, date_to)
     file_path = save(data, get_file_name(year, month))
-    print(f"Saved in: {file_path}")
+    print(f"Raw voting file saved in: {file_path}")
+
+def get_members(term=TERM) -> None:
+    data = get_voting(term)
+    file_path = save(data, get_members_file_name(term))
+    print(f"Members file saved in: {file_path}")
