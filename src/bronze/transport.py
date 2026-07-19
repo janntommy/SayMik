4d21ce5a7277905db890344c3c3de31f87b332c9
janntommy <tomekjanisiewi@gmail.com>
2026-07-19 15:41:16 +0000

    updated load_to_bronze function logic

--- src/bronze/transport.py
+++ src/bronze/transport.py
@@ -17,9 +17,12 @@ def transfer_to_unity(year: int, month: int) -> None:
 
 def load_to_bronze(year: int, month: int) -> None:
     file_path = f"{VOLUME_PATH}/{get_file_name(year, month)}"
+    table_name = f"db_saymik.bronze.votings_raw_{year}_{month:02d}"
+
     df = spark.read.json(file_path)
-    df.write.format("delta").mode("overwrite").saveAsTable("db_saymik.bronze.votings_raw")
-    print(f"Loaded {file_path} into db_saymik.bronze.votings_raw")
+    df.write.format("delta").mode("overwrite").saveAsTable(table_name)
+
+    print(f"Loaded {file_path} into {table_name}")
 
 
 transfer_to_unity(2026, 6)
