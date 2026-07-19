e27791220d5a26ef780a22e1c77c57c03df7f874
janntommy <tomekjanisiewi@gmail.com>
2026-07-19 15:10:19 +0000

    updated loading to bronze layer logic

--- src/databricks/transport.py
+++ src/databricks/transport.py
@@ -15,4 +15,12 @@ def transfer_to_unity(year: int, month: int) -> None:
 
     print(f"Data saved to: {file_path}")
 
+def load_to_bronze(year: int, month: int) -> None:
+    file_path = f"{VOLUME_PATH}/{get_file_name(year, month)}"
+    df = spark.read.json(file_path)
+    df.write.format("delta").mode("overwrite").saveAsTable("db_saymik.bronze.votings_raw")
+    print(f"Loaded {file_path} into db_saymik.bronze.votings_raw")
+
+
 transfer_to_unity(2026, 6)
+load_to_bronze(2026,6)
